e84880f76cec5e9f685052f5501263ac4228f66b
H4ck3rW0lf <sowa.db@gmail.com>
2025-02-20 19:43:01 +0100

    Added serach functionality

---
+++ BroadcastSocialMedia/BroadcastSocialMedia/Controllers/UsersController.cs
@@ -0,0 +1,28 @@
+﻿using BroadcastSocialMedia.Data;
+using BroadcastSocialMedia.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BroadcastSocialMedia.Controllers
+{
+    public class UsersController : Controller
+    {
+        private readonly ApplicationDbContext _dbContext; // Dependency Injection
+
+        public UsersController(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public async Task<IActionResult> Index(UsersIndexViewModel viewModel)
+        {
+            if(viewModel.Search != null)
+            {
+                var users = await _dbContext.Users.Where(u => u.Name.Contains(viewModel.Search))        // Hämtar lista på användare
+                    .ToListAsync();
+                viewModel.Result = users;
+            }
+            
+            return View(viewModel);
+        }
+    }
+}
